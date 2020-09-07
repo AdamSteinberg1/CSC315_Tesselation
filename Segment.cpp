c46f6d01a210a9ad4339ebd14725acf401095001
Adam L. Steinberg <steinberg_al@gsc_218_4.mercer.edu>
2020-09-07 12:24:44 -0400

    progress

---
+++ Segment.cpp
@@ -0,0 +1,7 @@
+#include "Segment.h"
+
+Segment::Segment(Vec2 p1, Vec2 p2)
+{
+  startPoint = p1;
+  endPoint = p2;
+}
---
+++ Segment.h
@@ -0,0 +1,9 @@
+#include "Vec2.h"
+//class for a line segment
+class Segment
+{
+  public:
+    Vec2 startPoint;
+    Vec2 endPoint;
+    Segment(Vec2 p1, Vec2 p2);
+};
